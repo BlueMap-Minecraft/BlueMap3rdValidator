15aa19d1ce76c1c8ba9a52386820dd9d3aad6378
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-09-22 04:49:14 +0200

    Removed testing dependencies, as there are no tests in this project

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,6 @@ repositories {
 }
 
 dependencies {
-	testImplementation(platform("org.junit:junit-bom:5.9.1"))
-	testImplementation("org.junit.jupiter:junit-jupiter")
 	implementation("org.spongepowered:configurate-hocon:4.2.0")
 }
 
