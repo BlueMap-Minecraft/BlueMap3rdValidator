d99338892797d2a279b0494fd7489d0a50fd7160
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-09-22 04:35:20 +0200

    Update to Configurate 4.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,12 @@ version = "1.0"
 
 repositories {
 	mavenCentral()
-	maven("https://repo.spongepowered.org/maven/")
 }
 
 dependencies {
 	testImplementation(platform("org.junit:junit-bom:5.9.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
-	implementation("org.spongepowered:configurate-hocon:4.2.0-SNAPSHOT")
+	implementation("org.spongepowered:configurate-hocon:4.2.0")
 }
 
 application {
